68536ec6cc3058a5e18ed4b6da8600b184d97619
Matteo Falkenberg <matteo.falkenberg@htl-wels.at>
2021-01-07 11:24:34 +0100

    initial structure

---
+++ src/model/CarDatabase.java
@@ -0,0 +1,9 @@
+/**class CarDatabase
+ * @author Matteo Falkenberg
+ * @version 1.0, 07.01.2021
+ */
+
+package model;
+
+public class CarDatabase {
+}
---
+++ src/model/Vehicle.java
@@ -0,0 +1,9 @@
+/**class Vehicle
+ * @author Matteo Falkenberg
+ * @version 1.0, 07.01.2021
+ */
+
+package model;
+
+public class Vehicle {
+}
